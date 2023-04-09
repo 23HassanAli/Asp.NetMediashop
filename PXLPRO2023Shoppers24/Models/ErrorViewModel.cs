f514ad16ec909ead5fd092dc55d8063987d33015
23HassanAli <94617327+23HassanAli@users.noreply.github.com>
2023-04-09 14:53:07 +0200

    Project startup

---
+++ PXLPRO2023Shoppers24/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PXLPRO2023Shoppers24.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
